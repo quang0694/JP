cc153b6de080ae9578f134aa8da5c7b28bb367b9
quang0694 <87409991+quang0694@users.noreply.github.com>
2021-08-31 13:30:30 +0900

    update

--- script.js
+++ script.js
@@ -2,31 +2,47 @@ let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () =>{
-
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
-
 }
 
 window.onscroll = () =>{
-
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
-
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
-  }else{
-    document.querySelector('#scroll-top').classList.remove('active');
-  }
-
 }
 
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
 }
 
-function fadeOut(){
-  setInterval(loader, 3000);
+function prev(){
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
 }
 
-window.onload = fadeOut();
+document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
+  image_1.addEventListener('click', () =>{
+    var src = image_1.getAttribute('src');
+    document.querySelector('.big-image-1').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-2').forEach(image_2 =>{
+  image_2.addEventListener('click', () =>{
+    var src = image_2.getAttribute('src');
+    document.querySelector('.big-image-2').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-3').forEach(image_3 =>{
+  image_3.addEventListener('click', () =>{
+    var src = image_3.getAttribute('src');
+    document.querySelector('.big-image-3').src = src;
+  });
+});
