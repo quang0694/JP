4fdcd2afd7389f6ae488f5cd53c9595a13285183
quang0694 <87409991+quang0694@users.noreply.github.com>
2021-09-03 11:48:44 +0900

    update

--- js/main.js
+++ js/main.js
@@ -42,7 +42,24 @@ $(document).ready(function(){
             }
         }
     });
-
+    /*-----------Screenshots Carousel-------------*/ 
+    $('.screenshots-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
 
 
 });
